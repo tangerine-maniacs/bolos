43987fcbb4227a8a40ec2b80a2406af4998aa5eb
Pablo Pérez Rodríguez <pablo@pabl.eu>
2022-11-08 22:35:09 +0100

    good form

--- bolos.c
+++ bolos.c
@@ -1,13 +1,12 @@
-#include <stdlib.h>
 #include <errno.h>
-#include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char *args[])
 {
-    return 0;
+  return 0;
 }
-
