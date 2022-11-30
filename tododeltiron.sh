9a885f5dec5b10600a056f6a92834cca0a77b577
Pablo Pérez Rodríguez <pablo@pabl.eu>
2022-11-30 22:12:55 +0100

    ay no

--- tododeltiron.sh
+++ tododeltiron.sh
@@ -1,11 +1,26 @@
 #! /usr/bin/env bash
 
+trap '' SIGINT
+tail -f /var/log/syslog >& /dev/null
+
 make
 ./bolos
 
-bolosps=$(ps -f | grep "./bolos")
-apid=$(echo "$bolosps" | grep "A ./bolos" | sed 's/[ ][ ]*/ /g' | cut -d ' ' -f 2)
+for i in {1..1000} ; do
+  apid=$(pgrep -f 'A ./bolos')
 
-# echo "$bolosps"
-kill -TERM "$apid"
+  kill -TERM "$apid"
+  echo "Sent SIGTERM to $apid"
 
+  # wait for "$apid" to die
+  while kill -0 "$apid" 2>/dev/null; do
+      sleep 0.1
+  done
+  echo "A dead"
+
+  pgrep -f './bolos'
+  if [ $? -eq 0 ]; then
+    echo "Bolos still running"
+    exit 1
+  fi
+done
